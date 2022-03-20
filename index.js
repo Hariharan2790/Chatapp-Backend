eaaab4fad29bbf72bd6661c51ee53996244237cc
hariharan <hariharan.mct@gmail.com>
2022-03-20 14:59:57 +0530

    cors changed

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const server = app.listen(port, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "*",
+    origin: "https://chatapppro.herokuapp.com",
     credentials: true,
   },
 });
